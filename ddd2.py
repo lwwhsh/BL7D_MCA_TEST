e619b9f1d2d82da2aa745ee4de473d2d66e77854
lwwhsh <lww@postech.ac.kr>
2016-09-16 20:01:39 +0900

    mcaTest.---

--- ddd2.py
+++ ddd2.py
@@ -22,17 +22,20 @@ import time
 class MainWindow(QMainWindow):
 
   # TODO: How can change max bins(energy) value.
+  # define X axis for mca bins
   x = numpy.arange(0, 2048, 1)
 
   cos = 0
   sin = 0
 
   # add LWW
-  commSignal = QtCore.pyqtSignal()
-  # self.commSignal.connect(self.plot)
+  # define and initialise signal for mca Done callback signal
+  commSignal = QtCore.pyqtSignal(object)
 
   def __init__(self):
     QMainWindow.__init__(self)
+
+    self.commSignal.connect(self.plot)
 
     self.figure  = matplotlib.pyplot.figure()
     self.drawing = self.figure.add_subplot(111)
@@ -51,46 +54,22 @@ class MainWindow(QMainWindow):
     self.dxpMca2PV = epics.PV('BL7D:dxpXMAP:mca2')
     self.dxpMca3PV = epics.PV('BL7D:dxpXMAP:mca3')
 
-    '''
-    def add_slider(foo, col):
-        sld = QSlider(Qt.Vertical, sliders)
-        sld.setFocusPolicy(Qt.NoFocus)
-        sld.valueChanged[int].connect(foo)
-        sld.valueChanged.connect(self.plot)
-        sliders_grid.addWidget(sld, 0, col)
-
-    add_slider(foo = self.set_cos, col = 0)
-    add_slider(foo = self.set_sin, col = 1)
-
-    dock.setWidget(sliders)
-    '''
     self.plot()
     self.addCallbackAcq()
 
   def onChanged(self, pvname=None, value=None, char_value=None, **kw):
-      print('onChanged: '), char_value, time.ctime()
-
+      # print('onChanged: '), char_value, time.ctime()
       if value is 1 : # value 1=Acquiring, 0=Done
           return
-
-      self.plot() # self.commSignal.connect(self.plot) # emit(self.plot())
+      # self.plot()
+      self.commSignal.emit(self.plot)
 
   def addCallbackAcq(self):
       # IMPORTANT: add_callback definition position. it define after callback function
       self.dxpAcqPV.add_callback(self.onChanged)
 
-  def set_cos(self, v):
-    self.cos = v / float(100)
-
-  def set_sin(self, v):
-    self.sin = v / float(100)
-
   def plot(self):
     self.drawing.hold(False)
-    # s = numpy.sin(self.x + self.sin)
-    # c = numpy.cos(self.x + self.cos)
-    # self.drawing.plot(self.x, s, 'r', self.x, c, 'g', self.x, s + c, 'b')
-    # self.drawing.set_ylim(-2, 2)
     s = self.dxpMca1PV.get()
     c = self.dxpMca2PV.get()
     b = self.dxpMca3PV.get()
@@ -98,7 +77,8 @@ class MainWindow(QMainWindow):
 
     self.drawing.plot(self.x, s, 'r', self.x, c, 'g', self.x, b, 'b')
     self.drawing.set_ylim(0, 2500)
-    self.drawing.set_xlim(500, 700) # added lww
+    self.drawing.set_xlim(550, 680) # Todo : please implement zoom in / zoom out.
+    self.drawing.grid()
     self.canvas.draw()
 
 if __name__ == "__main__":
