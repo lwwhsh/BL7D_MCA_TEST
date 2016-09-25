bb4c85a1eb36c6ebcf1082f16c824164f4a7ad55
lwwhsh <lww@postech.ac.kr>
2016-09-25 22:25:23 +0900

    mcaTest.---

--- ggg.py
+++ ggg.py
@@ -1,52 +1,48 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+import numpy as np
+import time
 
-class DraggablePoints(object):
-    def __init__(self, artists, tolerance=5):
-        for artist in artists:
-            artist.set_picker(tolerance)
-        self.artists = artists
-        self.currently_dragging = False
-        self.current_artist = None
-        self.offset = (0, 0)
+plt.ion()
 
-        for canvas in set(artist.figure.canvas for artist in self.artists):
-            canvas.mpl_connect('button_press_event', self.on_press)
-            canvas.mpl_connect('button_release_event', self.on_release)
-            canvas.mpl_connect('pick_event', self.on_pick)
-            canvas.mpl_connect('motion_notify_event', self.on_motion)
 
-    def on_press(self, event):
-        self.currently_dragging = True
+class DynamicUpdate():
+    # Suppose we know the x range
+    min_x = 0
+    max_x = 10
 
-    def on_release(self, event):
-        self.currently_dragging = False
-        self.current_artist = None
+    def on_launch(self):
+        # Set up plot
+        self.figure, self.ax = plt.subplots()
+        self.lines, = self.ax.plot([], [], 'o')
+        # Autoscale on unknown axis and known lims on the other
+        self.ax.set_autoscaley_on(True)
+        self.ax.set_xlim(self.min_x, self.max_x)
+        # Other stuff
+        self.ax.grid()
 
-    def on_pick(self, event):
-        if self.current_artist is None:
-            self.current_artist = event.artist
-            x0, y0 = event.artist.center
-            x1, y1 = event.mouseevent.xdata, event.mouseevent.ydata
-            self.offset = (x0 - x1), (y0 - y1)
+    def on_running(self, xdata, ydata):
+        # Update data (with the new _and_ the old points)
+        self.lines.set_xdata(xdata)
+        self.lines.set_ydata(ydata)
+        # Need both of these in order to rescale
+        self.ax.relim()
+        self.ax.autoscale_view()
+        # We need to draw *and* flush
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
 
-    def on_motion(self, event):
-        if not self.currently_dragging:
-            return
-        if self.current_artist is None:
-            return
-        dx, dy = self.offset
-        self.current_artist.center = event.xdata + dx, event.ydata + dy
-        self.current_artist.figure.canvas.draw()
+    # Example
+    # def __call__(self):
+    def __init__(self):
+        self.on_launch()
+        xdata = []
+        ydata = []
+        for x in np.arange(0,10,0.05):
+            xdata.append(x)
+            ydata.append(np.exp(-x**2)+10*np.exp(-(x-7)**2))
+            self.on_running(xdata, ydata)
+            time.sleep(1)
+        return xdata, ydata
 
-if __name__ == '__main__':
-    fig, ax = plt.subplots()
-    ax.set(xlim=[-1, 2], ylim=[-1, 2])
-
-    circles = [patches.Circle((0.32, 0.3), 0.2, fc='r', alpha=0.5),
-               patches.Circle((0.30, 0.3), 0.2, fc='g', alpha=0.5)]
-    for circ in circles:
-        ax.add_patch(circ)
-
-    dr = DraggablePoints(circles)
-    plt.show()
+d = DynamicUpdate()
+d()
